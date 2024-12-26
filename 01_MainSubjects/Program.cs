9136f7c36af963b3dc7b2432518d3031b299f342
Burakhan Küskü <burakhankusku@gmail.com>
2024-12-26 16:18:56 +0300

    Add files via upload

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Printing Commends
+            //Console.WriteLine("********* Food Categories **********");
+            //Console.WriteLine();
+            //Console.WriteLine("1-Soups");
+            //Console.WriteLine("2-Main Courses");
+            //Console.WriteLine("3-Cold Beginnings");
+            //Console.WriteLine("4-Salads");
+            //Console.WriteLine("5-Desserts");
+            //Console.WriteLine("6-Drinks");
+            //Console.WriteLine();
+            //Console.WriteLine("********* Food Categories **********");
+
+            //Console.ReadLine();
+            #endregion
+
+            #region String Variables
+
+            //string
+
+            //string name;
+            //name = "Jack";
+            //Console.Write(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Jack";
+            //customerSurname = "Chan";
+            //customerPhone = "00544 58421 565955 565";
+            //customerEmail = "mail@mail.com";
+            //district = "Orlanda";
+            //city = "United States";
+
+            //Console.WriteLine("***** Rezervation Card *****");
+            //Console.WriteLine();
+            //Console.WriteLine("Customer: " + customerName + " " + customerSurname);
+            //Console.WriteLine("Communication : " + customerPhone);
+            //Console.WriteLine("Email Adress : " + customerEmail);
+            //Console.WriteLine("Adress : " + district + " " + city);
+
+
+
+            //Console.Read();
+
+            #endregion
+
+            #region Int Variables
+
+            //int number = 24;
+            //Console.WriteLine(number);
+
+            //int burgerPrice = 270;
+            //int frizePrice = 50;
+            //int pizzaPrize = 300;
+            //int cokePrize = 40;
+            //int lemonadePrize = 50;
+            //int waterPrice = 12;
+
+
+
+            //Console.WriteLine("***** Restaurant Menu *****");
+            //Console.WriteLine();
+
+            //Console.WriteLine("Hamburger : " + burgerPrice);
+            //Console.WriteLine("Frize : " + frizePrice);
+            //Console.WriteLine("Pizza : " + pizzaPrize);
+            //Console.WriteLine("Coke  : " + cokePrize);
+            //Console.WriteLine("Lemonade : " + lemonadePrize);
+            //Console.WriteLine("Water : " + waterPrice);
+
+            //Console.WriteLine();
+
+
+            //Console.WriteLine("------------- Account ----------");
+            //Console.WriteLine();
+            //int burgerCount;
+            //int frizeCount;
+            //int pizzaCount;
+            //int cokeCount;
+            //int lemonadeCount;
+            //int waterCount;
+
+            //int totalBurgerPrize = 0;
+            //int totalfrizePrize = 0;
+            //int totalpizzaPrize = 0;
+            //int totalcokePrize = 0;
+            //int totallemonadePrize = 0;
+            //int totalwaterPrize = 0;
+
+            //burgerCount = 3;
+            //frizeCount = 3;
+            //pizzaCount = 1;
+            //cokeCount = 2;
+            //lemonadeCount = 2;
+            //waterCount = 3;
+
+
+            //totalBurgerPrize = burgerCount * burgerPrice;
+            //totalfrizePrize = frizeCount * frizePrice;
+            //totalpizzaPrize = pizzaCount * pizzaPrize;
+            //totalcokePrize = cokeCount * cokePrize;
+            //totallemonadePrize = lemonadeCount * lemonadePrize;
+            //totalwaterPrize = waterCount * waterPrice;
+
+            //Console.WriteLine("Total Burger Prize" + " (3 pieces) :  " + totalBurgerPrize);
+            //Console.WriteLine("Total Frize Prize" + " (3 pieces) :  " + totalfrizePrize);
+            //Console.WriteLine("Total Pizza Prize" + " (1 pieces) :  " + totalpizzaPrize);
+            //Console.WriteLine("Total Coke Prize" + " (2 pieces) :  " + totalcokePrize);
+            //Console.WriteLine("Total Lemonade Prize" + " (2 pieces) :  " + totallemonadePrize);
+            //Console.WriteLine("Total Water Prizes" + " (3 pieces) :  " + totalwaterPrize);
+            //Console.WriteLine();
+
+            //int totalPrize = 0;
+
+            //totalPrize = totalBurgerPrize + totalfrizePrize + totalpizzaPrize + totalcokePrize + totallemonadePrize + totalwaterPrize;
+
+            //Console.WriteLine("Total Prize : " + totalPrize);
+
+
+            //Console.Read();
+
+            #endregion
+
+        }
+    }
+}
